40f87baef1f6aa369609f7acf5846db700ba532c
Umeigbo Obumneme Steven <obumneme.umeigbo@decagon.dev>
2021-09-25 09:40:56 +0100

    first commit

--- src/main/java/com/example/blogger/exception/PostNotFoundException.java
+++ src/main/java/com/example/blogger/exception/PostNotFoundException.java
@@ -1,7 +1,10 @@
 package com.example.blogger.exception;
 
-import java.text.MessageFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.text.MessageFormat;
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class PostNotFoundException extends RuntimeException{
     public PostNotFoundException(final Long id){
         super(MessageFormat.format("Could not find cart with id: {0}", id));
--- src/main/java/com/example/blogger/exception/UserNotFoundException.java
+++ src/main/java/com/example/blogger/exception/UserNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.text.MessageFormat;
-
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException{
 
     public UserNotFoundException(final Long id){
